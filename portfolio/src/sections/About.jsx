a1bb94bcf601c54ae2f3d052b611d9df24ccdf22
vrtx <jose.gomez4@eia.edu.co>
2024-09-29 10:53:40 -0500

    About upgraded

--- portfolio/src/sections/About.jsx
+++ portfolio/src/sections/About.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Globe from "react-globe.gl";
 
 const About = () => {
 
@@ -13,9 +14,37 @@ const About = () => {
                             <p className="grid-subtext">mucho texto</p>
                         </div>
                     </div>
-
-
                 </div>
+                <div className="col-span-1 xl:row-span3">
+                    <div className="grid-container">
+                        <img src="/assets/grid2.png" alt="grid-2" className="w-full sm:h-[276px] h-fit object-contain"/>
+                        <div>
+                            <p className="grid-headtext">texto</p>
+                            <p className="grid-subtext">mucho texto</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-span-1 xl:row-span-4">
+                    <div className="grid-container">
+                        <div className="rounded-3xl w-full sm:h-[326px] h-fit flex justify-center items-center">
+                            <Globe
+                                height={326}
+                                width={326}
+                                backgroundColor="rgba(0,0,0,0)"
+                                backgroundImageOpacity={0.5}
+                                showAtmosphere
+                                showGraticules
+                                globeImageUrl="//unpkg.com/three-globe/example/img/earth-day.jpg"
+                                bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
+                            />
+                        </div>
+                        <div>
+                            <p className="grid-headtext">texto</p>
+                            <p className="grid-subtext">mucho texto</p>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </section>
     )
