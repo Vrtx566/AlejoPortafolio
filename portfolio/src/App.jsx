22794b9a5efdad28ebc05efded78f4aba0bc30c9
vrtx <jose.gomez4@eia.edu.co>
2024-10-15 23:02:08 -0500

    finished

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Navbar from "./sections/Navbar.jsx"
+import Hero from "./sections/Hero.jsx"
+import About from "./sections/About.jsx"
+import Projects from "./sections/Projects.jsx"
+import Contact from "./sections/Contact.jsx";
+import Footer from "./sections/Footer.jsx";
+
+const App = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Hero/>} />
+                <Route path="/about" element={
+                    <>
+                        <Navbar/>
+                        <About/>
+                        <Projects/>
+                        <Contact/>
+                        <Footer/>
+                    </>
+                } />
+            </Routes>
+        </Router>
+    )
+}
+
+export default App
